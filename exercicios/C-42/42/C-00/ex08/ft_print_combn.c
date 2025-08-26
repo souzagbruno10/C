4e21fbe116f38f58b9c49e781ae7e78bf90179b1
Bruno Gomes de Souza <souzagbruno10@gmail.com>
2025-08-26 07:17:58 -0300

    nova pasta

---
+++ exercicios/C-42/42/C-00/ex08/ft_print_combn.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brunogomesdesouza <brunogomesdesouza@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/19 17:30:26 by brunogomesd       #+#    #+#             */
+/*   Updated: 2025/07/19 17:36:00 by brunogomesd      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_print_combn(int n)
+{
+    
+}
+
+int main(void)
+{
+    
+
+
+
+    return (0);
+}
---
+++ exercicios/C-42/42/C-01/ex02/ft_swap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brunogomesdesouza <brunogomesdesouza@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/25 18:32:15 by brunogomesd       #+#    #+#             */
+/*   Updated: 2025/08/25 18:52:17 by brunogomesd      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_swap( int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+  
+    
+}
+
+#include <stdio.h>
+
+int main (void)
+{
+    int x, y;
+
+    x = 42;
+    y = 8;
+
+    ft_swap(&x, &y);
+    printf("x é 42 mas vira %d /", x);
+    printf("y é 8 mas vira %d ", y);
+
+    return 0 ;
+}
