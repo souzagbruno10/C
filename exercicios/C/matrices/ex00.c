33cef1b6c82b555fc139f765415499e13924635f
Bruno Gomes de Souza <souzagbruno10@gmail.com>
2025-09-03 06:31:21 -0300

    ïnicio exercicios matrizes

---
+++ exercicios/C/matrices/ex00.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int points[4][4] = {
+		
+		{7,3,4,5},
+		{10,22,31,42},
+		{11,2,0,100},
+		{90,24,11,23}
+		
+	};
+
+	printf("%d", points[0][0]);
+
+
+	return 0;
+}
